aa9776c78e6e01b388e4c5ecb74d2210700ffe6c
osinterr <maska0218@gmail.com>
2022-04-01 03:57:10 -0400

    works

---
+++ built/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pihtaw/Documents/programming/tp2/hw_task2
+# Build directory: /home/pihtaw/Documents/programming/tp2/hw_task2/built
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/pihtaw/Documents/programming/tp2/hw_task2/built/tests[1]_include.cmake")
